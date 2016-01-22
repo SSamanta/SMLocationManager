7265fe33ef4d346f104a5c3204aa9139d129fc4d
Susim <Susim@susims-imac.corp.capgemini.com>
2016-01-22 15:05:12 +0530

    Created Project

---
+++ SMLocationManager/SMLocationManager/SMLocationManager.h
@@ -0,0 +1,19 @@
+//
+//  SMLocationManager.h
+//  SMLocationManager
+//
+//  Created by Susim Samanta on 22/01/16.
+//  Copyright © 2016 Susim Samanta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMLocationManager.
+FOUNDATION_EXPORT double SMLocationManagerVersionNumber;
+
+//! Project version string for SMLocationManager.
+FOUNDATION_EXPORT const unsigned char SMLocationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMLocationManager/PublicHeader.h>
+
+
